e2e40919e320e1d060b5a2ef148232c0b1a15a68
damirachimovici <achimovici.damir96@gmail.com>
2020-11-26 10:40:22 +0200

    Header

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBKozBObSUuT5W-1Wqx7QD0iE0rhP2sfn8",
+    authDomain: "my-facebook-c0104.firebaseapp.com",
+    databaseURL: "https://my-facebook-c0104.firebaseio.com",
+    projectId: "my-facebook-c0104",
+    storageBucket: "my-facebook-c0104.appspot.com",                 /*copied from project settings*/
+    messagingSenderId: "225597095450",
+    appId: "1:225597095450:web:d466b31c2fb8be43e19ba3",
+    measurementId: "G-P2JH8EJJE8"
+};
